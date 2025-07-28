88eeb7160368a4f3eadfa0d6ffdfa38b7dc95d68
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2025-07-27 22:17:27 -0700

    killing myself

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,6 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-module.exports = {
-  rules: {
-   "@next/next/no-img-element": "off",
-  },
-};
-
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
